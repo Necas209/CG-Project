af9aaf46993e17177d0f384a3852b7c3692a9903
Diogo Medeiros <diogo_medeiros@sapo.pt>
2022-05-23 23:12:09 +0100

    House + Fence + Trees

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ function main() {
 
 	const scene = new THREE.Scene();
 
+	const axesHelper = new THREE.AxesHelper(5);
+	axesHelper.position.setY(10);
+	scene.add(axesHelper);
+
 	const camera = new THREE.PerspectiveCamera(
 		75,
 		window.innerWidth / window.innerHeight,
@@ -46,7 +50,7 @@ function main() {
 
 	const worldOctree = new Octree();
 
-	const playerCollider = new Capsule(new THREE.Vector3(-5, 0.35, -1), new THREE.Vector3(-5, 1, -1), 0.35);
+	const playerCollider = new Capsule(new THREE.Vector3(0, 0.5, 10), new THREE.Vector3(0, 2.3, 10), 0.2);
 
 	const playerVelocity = new THREE.Vector3();
 	const playerDirection = new THREE.Vector3();
@@ -134,8 +138,8 @@ function main() {
 
 	function teleportPlayerIfOob() {
 		if (camera.position.y <= -5) {
-			playerCollider.start.set(-5, 0.35, -1);
-			playerCollider.end.set(-5, 1, -1);
+			playerCollider.start.set(0, 0.5, 10);
+			playerCollider.end.set(0, 2, 10);
 			playerCollider.radius = 0.35;
 			camera.position.copy(playerCollider.end);
 			camera.rotation.set(0, 0, 0);
@@ -143,61 +147,161 @@ function main() {
 	}
 
 	// Ground
-	const planeGeometry = new THREE.PlaneGeometry(3000, 3000);
+	const planeGeometry = new THREE.PlaneGeometry(50, 50);
 	const tex_loader = new THREE.TextureLoader();
-	const color_map = tex_loader.load('objects/creepy-house-gltf/textures/ground2_diffuse.png');
+	const color_map = tex_loader.load('textures/ground/Ground024_1K_Color.png');
 	color_map.wrapS = THREE.RepeatWrapping;
 	color_map.wrapT = THREE.RepeatWrapping;
 	color_map.repeat.set(10, 10);
-	const material = new THREE.MeshPhysicalMaterial({
+	const ground_material = new THREE.MeshStandardMaterial({
 		map: color_map,
-		aoMap: tex_loader.load('objects/creepy-house-gltf/textures/ground2_occlusion.png'),
-		normalMap: tex_loader.load('objects/creepy-house-gltf/textures/ground2_normal.png'),
-		specularColorMap: tex_loader.load('objects/creepy-house-gltf/textures/ground2_specularGlossiness.png'),
+		aoMap: tex_loader.load('textures/ground/Ground024_1K_AmbientOcclusion.png'),
+		normalMap: tex_loader.load('textures/ground/Ground024_1K_NormalGL.png'),
+		roughnessMap: tex_loader.load('textures/ground/Ground024_1K_Roughness.png'),
+		displacementMap: tex_loader.load('textures/ground/Ground024_1K_Displacement.png')
 	});
-	const ground = new THREE.Mesh(planeGeometry, material);
-	ground.position.setY(-155);
+	const ground = new THREE.Mesh(planeGeometry, ground_material);
 	ground.rotateX(-Math.PI / 2);
-	scene.add(ground);
-				
-	// Trees
-	let tree = undefined;
+	const world_objs = new THREE.Group();
+	world_objs.add(ground);
+
+	// House
+	// const gltf_loader = new GLTFLoader();
+	// gltf_loader.load('objects/dachniy_house/scene.gltf', (gltf) => {
+	// 	gltf.scene.scale.set(0.03, 0.03, 0.03);
+	// 	gltf.scene.rotateY(Math.PI);
+	// 	gltf.scene.position.setY(0.4);
+	// 	gltf.scene.traverse(child => {
+	// 		if (child.isMesh) {
+	// 			child.castShadow = true;
+	// 			child.receiveShadow = true;
+	// 		}
+	// 	});
+	// 	world_objs.add(gltf.scene);
+	// 	worldOctree.fromGraphNode(world_objs);
+	// });
 	const gltf_loader = new GLTFLoader();
-	gltf_loader.load('objects/pine_tree/scene.gltf', (gltf) => {
-		scene.add(gltf.scene);
-		gltf.scene.position.set(1000, -155, 1000);
-		tree = gltf.scene;
+	gltf_loader.load('objects/old_house/scene.gltf', (gltf) => {
+		gltf.scene.scale.set(0.015, 0.015, 0.015);
+		//gltf.scene.rotateY(Math.PI);
+		gltf.scene.position.set(-4, 0.4, 1);
 		gltf.scene.traverse(child => {
 			if (child.isMesh) {
 				child.castShadow = true;
 				child.receiveShadow = true;
-				if (child.material.map) {
-					child.material.map.anisotropy = 4;
-				}
 			}
 		});
+		world_objs.add(gltf.scene);
+		worldOctree.fromGraphNode(world_objs);
 	});
 	
-	// Creepy house
-	gltf_loader.load('objects/creepy-house-gltf/scene.gltf', (gltf) => {
-		gltf.scene.scale.set(0.02, 0.02, 0.02);
-		scene.add(gltf.scene);
-		gltf.scene.add(ground);
-		gltf.scene.add(tree);
-		worldOctree.fromGraphNode(gltf.scene);
+	// Picket fence
+	const shape = new THREE.Shape();
+	shape.moveTo(0, 0);
+	shape.lineTo(0.2, 0);
+	shape.lineTo(0.2, 0.9);
+	shape.lineTo(0.7, 0.9);
+	shape.lineTo(0.7, 1.1);
+	shape.lineTo(0.2, 1.1);
+	shape.lineTo(0.2, 1.5);
+	shape.lineTo(0.7, 1.5);
+	shape.lineTo(0.7, 1.7);
+	shape.lineTo(0.2, 1.7);
+	shape.lineTo(0.2, 2);
+	shape.lineTo(0.1, 2.25);
+	shape.lineTo(0, 2);
+	shape.lineTo(0, 1.7);
+	shape.lineTo(-0.5, 1.7);
+	shape.lineTo(-0.5, 1.5);
+	shape.lineTo(0, 1.5);
+	shape.lineTo(0, 1.1);
+	shape.lineTo(-0.5, 1.1);
+	shape.lineTo(-0.5, 0.9);
+	shape.lineTo(0, 0.9);
+	shape.lineTo(0, 0);
+
+	const geometry = new THREE.ExtrudeGeometry(shape, { depth: 0 });
+	const map = tex_loader.load('textures/fence/Wood033_1K_Color.png');
+	map.wrapS = map.wrapT = THREE.RepeatWrapping;
+	map.repeat.set(0.5, 0.5);
+	const normalMap = tex_loader.load('textures/fence/Wood033_1K_NormalGL.png');
+	normalMap.wrapS = normalMap.wrapT = THREE.RepeatWrapping;
+	normalMap.repeat.set(0.5, 0.5);
+	const displacementMap = tex_loader.load('textures/fence/Wood033_1K_Displacement.png');
+	displacementMap.wrapS = displacementMap.wrapT = THREE.RepeatWrapping;
+	displacementMap.repeat.set(0.5, 0.5);
+
+	const fence_material = new THREE.MeshPhysicalMaterial({
+		map: map,
+		aoMap: tex_loader.load('textures/fence/Wood033_1K_AmbientOcclusion.png'),
+		normalMap: normalMap,
+		roughnessMap: tex_loader.load('textures/fence/Wood033_1K_Roughness.png'),
+		displacementMap: displacementMap,
+		displacementScale: 0,
+	});
+
+	const fences = new THREE.Group();
+	const fence = new THREE.Mesh(geometry, fence_material);
+	for (let i = 0; i < 14; i++) {
+		const temp = fence.clone();
+		temp.position.set(-10 + 1.5 * i, 0, -10);
+		fences.add(temp);
+	}
+	for (let i = 0; i < 14; i++) {
+		const temp = fence.clone();
+		temp.rotateY(Math.PI / 2);
+		temp.position.set(-10, 0, -10 + 1.5 * i);
+		fences.add(temp);
+	}
+	for (let i = 0; i < 14; i++) {
+		if (i != 7)
+		{
+			const temp = fence.clone();
+			temp.position.set(-10 + 1.5 * i, 0, 10);
+			fences.add(temp);
+		}
+	}
+	for (let i = 0; i < 14; i++) {
+		const temp = fence.clone();
+		temp.rotateY(Math.PI / 2);
+		temp.position.set(10, 0, -10 + 1.5 * i);
+		fences.add(temp);
+	}
+	world_objs.add(fences);
+
+	// Trees
+	const treePositions = [
+		[15, 0, -15], [13, 0, -9], [15, 0, -1],
+		[14.5, 0, 0], [13.5, 0, 10], [15, 0, 18],
+		[8, 0, 13.5], [-1, 0, 17], [-5, 0, 15],
+		[-13, 0, 15], [-17, 0, 6], [-15, 0, 4], 
+		[-14, 0, -1], [-11, 0, -15], [-13.5, 0, -13],
+		[-10, 0, -17], [-6, 0, -13], [0, 0, -13.5],
+		[1, 0, -15], [8, 0, -14]
+	];
+	gltf_loader.load('objects/pine_tree/scene.gltf', (gltf) => {
+		gltf.scene.scale.set(0.04, 0.04, 0.04);
 		gltf.scene.traverse(child => {
 			if (child.isMesh) {
 				child.castShadow = true;
 				child.receiveShadow = true;
-				if (child.material.map) {
-					child.material.map.anisotropy = 4;
-				}
 			}
 		});
+		const trees = new THREE.Group();
+		for(let i = 0; i < 20; i++)
+		{
+			const tree = gltf.scene.clone();
+			tree.position.set(treePositions[i][0], treePositions[i][1], treePositions[i][2]);
+			trees.add(tree);
+		}
+		world_objs.add(trees);
 	});
-		
+
+	scene.add(world_objs);
+	worldOctree.fromGraphNode(world_objs);
+
 	// Ambient light
-	const light = new THREE.AmbientLight(0xFFFFE0, 1);
+	const light = new THREE.AmbientLight(0xFFFFE0, 1.5);
 	scene.add(light);
 
 	// Background - Night Sky
